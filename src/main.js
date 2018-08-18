bf22466af620c7622e4630ec0cae7e2d66f83248
Sophiawangxp <sophia.wangxp@gmail.com>
2018-08-18 19:34:09 +1000

    Add Element-UI

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './plugins/element.js'
+import ElementUI from 'element-ui';
 import router from './router'
 
 Vue.config.productionTip = false
 
+Vue.use(ElementUI);
 new Vue({
   router,
   render: h => h(App)
