438adae2e12c1db6499a4db3508d7bdc77a405be
narrowei <exiawei@gmail.com>
2018-09-24 18:06:58 +1000

    week 9 tutorial warm up task

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -63,10 +63,13 @@
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
+    body {
+        background: #ff0;
+    }
 </style>
