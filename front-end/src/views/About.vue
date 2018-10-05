f4077dead94608ed8e6ab4b10769625ea67656f2
MikeYCM <42535817+MikeYCM@users.noreply.github.com>
2018-10-05 18:08:01 +1000

    Update About.vue

--- front-end/src/views/About.vue
+++ front-end/src/views/About.vue
@@ -1,5 +1,40 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    
   </div>
 </template>
+
+<html>
+<head> About </head>
+
+<body>
+
+<h1> My tutorial </h1>
+<p1> My-Tutorials is a platform where students could seek for tutorials as well 
+as tutors could register tutorials. The website is lightweight, interactive, RESTful, 
+could support reasonable size Database. The front-end and back-end are powered by Vue.JS and Node.JS respectively.
+</p1>
+<h2> Features </h2>
+<p2>
+User sign-up and log-in/log-out
+Different classes of User have different profile page
+Students browse through tutorials
+Tutors register new tutorials
+Students enroll in selected tutorials
+Students check out payment
+</p2>
+
+<h3> Technologies  </h3>
+<p3>
+My-Tutorials uses a number of open source projects to work properly:
+Vue.js - The Progressive JavaScript Framework
+Node.js - Asynchronous event driven JavaScript runtime for the back-end
+Express - Fast Node.js web application framework
+Webpack - A static module bundler for modern JavaScript applications
+Babel - A JavaScript compiler
+Element - Vue 2.0 based component library
+</p3>
+
+
+</body>
